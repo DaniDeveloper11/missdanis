237d1d7b3592b97b57956a8aff530d01c5297fa0
DaniDeveloper11 <151704893+DaniDeveloper11@users.noreply.github.com>
2026-03-04 23:07:54 -0600

    add: some stufs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 import Alpine from 'alpinejs'
+import axios from 'axios'
+
+window.axios = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+})
+
+window.BACKEND_URL = import.meta.env.VITE_API_URL.replace('/api', '')
 
 window.Alpine = Alpine
+
+Alpine.data('catalog', () => ({
+  products: [],
+  async fetchCatalog() {
+    try {
+      const response = await window.axios.get('/products?populate=*')
+      this.products = response.data.data
+    } catch (error) {
+      console.error('Error fetching products:', error)
+    }
+  },
+  init() {
+    this.fetchCatalog()
+  }
+}))
 
 Alpine.start()
